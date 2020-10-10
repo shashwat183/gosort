3d4a04b8ac61f2205bccb6cfceac8c01c7ebb2c3
shashwat pragya <shashwat183@gmail.com>
2020-10-11 00:34:16 +0100

    add sort files

---
+++ sort/quicksort.go
@@ -0,0 +1,7 @@
+package sort
+
+// Quicksort function takes as input an int slice and returns the the sorted slice.
+// It uses the quick-sort algorithm to sort the array
+func Quicksort(array []int) []int {
+	return array
+}
