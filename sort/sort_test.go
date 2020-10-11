97d492eabcd5b55f4fa0885939b4e68c99d1beef
shashwat pragya <shashwat183@gmail.com>
2020-10-12 00:07:49 +0100

    Write merge sort

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,7 +1,9 @@
-package sort
+package sort_test
 
 import (
 	"testing"
+
+	"github.com/shashwat183/gosort/sort"
 )
 
 // test for selection sort
@@ -10,28 +12,34 @@ func TestSelectionsort(t *testing.T) {
 	// test for long array with no repeated numbers
 	array := []int{1, 4, 5, 3, 6, 9}
 	sorted := []int{1, 3, 4, 5, 6, 9}
-	got := Selectionsort(array)
+	got := sort.Selectionsort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 
 	// test for long array with repeated elements
 	array = []int{1, 4, 4, 5, 3, 6, 5, 9}
 	sorted = []int{1, 3, 4, 4, 5, 5, 6, 9}
-	got = Selectionsort(array)
+	got = sort.Selectionsort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 
 	// test for array with 1 element
 	array = []int{4}
 	sorted = []int{4}
-	got = Selectionsort(array)
+	got = sort.Selectionsort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 }
 
@@ -41,28 +49,82 @@ func TestBubblesort(t *testing.T) {
 	// test for long array with no repeated numbers
 	array := []int{1, 4, 5, 3, 6, 9}
 	sorted := []int{1, 3, 4, 5, 6, 9}
-	got := Bubblesort(array)
+	got := sort.Bubblesort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 
 	// test for long array with repeated elements
 	array = []int{1, 4, 4, 5, 3, 6, 5, 9}
 	sorted = []int{1, 3, 4, 4, 5, 5, 6, 9}
-	got = Bubblesort(array)
+	got = sort.Bubblesort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 
 	// test for array with 1 element
 	array = []int{4}
 	sorted = []int{4}
-	got = Bubblesort(array)
+	got = sort.Bubblesort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
+	}
+}
+
+// test for merge sort
+func TestMergesort(t *testing.T) {
+
+	// test for long array with no repeated numbers
+	array := []int{1, 4, 5, 3, 6, 9}
+	sorted := []int{1, 3, 4, 5, 6, 9}
+	got := sort.Mergesort(array)
+
+	if !compareslices(got, sorted) {
+		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
+	}
+
+	// test for long array with repeated elements
+	array = []int{1, 4, 4, 5, 3, 6, 5, 9}
+	sorted = []int{1, 3, 4, 4, 5, 5, 6, 9}
+	got = sort.Mergesort(array)
+
+	if !compareslices(got, sorted) {
+		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
+	}
+
+	// test for array with 1 element
+	array = []int{4}
+	sorted = []int{4}
+	got = sort.Mergesort(array)
+
+	if !compareslices(got, sorted) {
+		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
+	}
+
+	// test for array with descending sort
+	array = []int{5, 4, 3, 2, 1}
+	sorted = []int{1, 2, 3, 4, 5}
+	got = sort.Mergesort(array)
+
+	if !compareslices(got, sorted) {
+		t.Errorf("array not sorted correctly %v", got)
+	} else {
+		t.Logf("array sorted successfully %v", got)
 	}
 }
 
