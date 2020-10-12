b6cea7935da02b1cb441819c07c9a1a3a68ef280
shashwat pragya <shashwat183@gmail.com>
2020-10-12 20:33:42 +0100

    fix tests

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -134,7 +134,7 @@ func TestQuicksort(t *testing.T) {
 	// test for long array with no repeated numbers
 	array := []int{1, 4, 5, 3, 6, 9}
 	sorted := []int{1, 3, 4, 5, 6, 9}
-	got := Quicksort(array)
+	got := sort.Quicksort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
@@ -143,7 +143,7 @@ func TestQuicksort(t *testing.T) {
 	// test for long array with repeated elements
 	array = []int{1, 4, 4, 5, 3, 6, 5, 9}
 	sorted = []int{1, 3, 4, 4, 5, 5, 6, 9}
-	got = Quicksort(array)
+	got = sort.Quicksort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
@@ -152,7 +152,7 @@ func TestQuicksort(t *testing.T) {
 	// test for array with 1 element
 	array = []int{4}
 	sorted = []int{4}
-	got = Quicksort(array)
+	got = sort.Quicksort(array)
 
 	if !compareslices(got, sorted) {
 		t.Errorf("array not sorted correctly %v", got)
